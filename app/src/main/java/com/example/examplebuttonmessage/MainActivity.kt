fc4fe34ac87be7e65fa5a94b69193266a93a35b1
ItsMarioArt <l23590231@sjuanrio.tecnm.mx>
2025-02-14 04:12:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/examplebuttonmessage/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.examplebuttonmessage
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var counterButton: Button
+    var contador = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        counterButton = findViewById(R.id.counterButton)
+
+        counterButton.setOnClickListener {
+            contador++
+            counterButton.text = "Presionaste $contador veces."
+        }
+    }
+}
